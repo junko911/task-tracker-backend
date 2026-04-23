ad009cf8e6eaf80c51d286ce2b8b7f0be7828911
Junko Tahara <junkotahara911@gmail.com>
2026-04-23 13:14:43 -0400

    configure development environment to allow requests from Docker host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Requests from the host machine to the API container use this host (Docker Desktop).
+  config.hosts << "host.docker.internal"
 end
